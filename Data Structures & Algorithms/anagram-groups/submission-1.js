7c42cfda55c15f058e7864e16451e1a0c5f5f9b3
ANEES ABOOBACKER <167506602+ANESSABO0421@users.noreply.github.com>
2026-09-06 13:31:05 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let group={}
+        for(let i=0;i<strs.length;i++){
+            let currentword=strs[i]
+            let count={}
+            
+            
+            for(let j=0;j<currentword.length;j++){
+                let char=currentword[j]
+                if(count[char]){
+                    count[char]=count[char]+1
+                }else{
+                    count[char]=1
+                }
+            }
+
+            let key = ""
+
+            for (let code = 97; code <= 122; code++) {
+                // ascii
+                let char = String.fromCharCode(code)
+
+                //  count
+                //  ├── a → 1  ✅
+                //  ├── c → 1
+                //  └── t → 1
+                if (count[char]) {
+                    key = key + char + count[char]
+                }
+            }
+
+            if(group[key]){
+                group[key].push(currentword)
+            }else{
+                group[key]=[currentword]
+            }
+
+        }
+
+        let out=[]
+
+        for(let key in group){
+            out.push(group[key])
+        }
+
+        return out
+
+    }
+}
